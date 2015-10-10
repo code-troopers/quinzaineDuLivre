745b4abe313955127bb35d0a1683dc98f41c1692
Matthieu Bollot <m.bollot@code-troopers.com>
2015-10-10 13:27:10 +0200

    Added infinite scroll in table directive (not filtered)

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -7,6 +7,28 @@ angular.module('quinzaineDuLivreApp')
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
     })
+    .controller('TableListeController', function ($scope, Principal, Livre, ParseLinks) {
+        $scope.livres = [];
+        $scope.page = 0;
+        $scope.loadAll = function() {
+            Livre.query({page: $scope.page, size: 20}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                for (var i = 0; i < result.length; i++) {
+                    $scope.livres.push(result[i]);
+                }
+            });
+        };
+        $scope.reset = function() {
+            $scope.page = 0;
+            $scope.livres = [];
+            $scope.loadAll();
+        };
+        $scope.loadPage = function(page) {
+            $scope.page = page;
+            $scope.loadAll();
+        };
+        $scope.loadAll();
+    })
     .directive('filtreTable', function () {
         return {
             restrict: 'E',
@@ -16,6 +38,7 @@ angular.module('quinzaineDuLivreApp')
     .directive('tableListe', function () {
         return {
             restrict: 'E',
-            templateUrl: 'scripts/app/main/table.html'
+            templateUrl: 'scripts/app/main/table.html',
+            controller: 'TableListeController'
         };
     });
