94fb6cb8f121ce402893744d0d1600c6fcf2f3e6
Florian Chauveau <f.chauveau@code-troopers.com>
2015-10-10 12:36:49 +0200

    Firsts display steps

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('quinzaineDuLivreApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate', 
+angular.module('quinzaineDuLivreApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pascalprecht.translate',
                'ui.bootstrap', // for modal dialogs
     'ngResource', 'ui.router', 'ngCookies', 'ngAria', 'ngCacheBuster', 'ngFileUpload', 'infinite-scroll'])
 
@@ -14,12 +14,12 @@ angular.module('quinzaineDuLivreApp', ['LocalStorageModule', 'tmh.dynamicLocale'
             if (Principal.isIdentityResolved()) {
                 Auth.authorize();
             }
-            
+
             // Update the language
             Language.getCurrent().then(function (language) {
                 $translate.use(language);
             });
-            
+
         });
 
         $rootScope.$on('$stateChangeSuccess',  function(event, toState, toParams, fromState, fromParams) {
@@ -38,12 +38,12 @@ angular.module('quinzaineDuLivreApp', ['LocalStorageModule', 'tmh.dynamicLocale'
             if (toState.data.pageTitle) {
                 titleKey = toState.data.pageTitle;
             }
-            
+
             $translate(titleKey).then(function (title) {
                 // Change window title with translated one
                 $window.document.title = title;
             });
-            
+
         });
 
         $rootScope.back = function() {
@@ -85,13 +85,13 @@ angular.module('quinzaineDuLivreApp', ['LocalStorageModule', 'tmh.dynamicLocale'
         $httpProvider.interceptors.push('authExpiredInterceptor');
         $httpProvider.interceptors.push('authInterceptor');
         $httpProvider.interceptors.push('notificationInterceptor');
-        
+
         // Initialize angular-translate
         $translateProvider.useLoader('$translatePartialLoader', {
             urlTemplate: 'i18n/{lang}/{part}.json'
         });
 
-        $translateProvider.preferredLanguage('en');
+        $translateProvider.preferredLanguage('fr');
         $translateProvider.useCookieStorage();
         $translateProvider.useSanitizeValueStrategy('escaped');
         $translateProvider.addInterpolation('$translateMessageFormatInterpolation');
@@ -99,5 +99,5 @@ angular.module('quinzaineDuLivreApp', ['LocalStorageModule', 'tmh.dynamicLocale'
         tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
         tmhDynamicLocaleProvider.useCookieStorage();
         tmhDynamicLocaleProvider.storageKey('NG_TRANSLATE_LANG_KEY');
-        
+
     });
