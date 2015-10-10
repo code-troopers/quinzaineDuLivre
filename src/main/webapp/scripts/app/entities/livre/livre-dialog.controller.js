6d768e5a166031b2aa6256f586e2f3a0f30724b1
Florian Chauveau <f.chauveau@code-troopers.com>
2015-10-10 20:22:42 +0200

    updage factories get

--- src/main/webapp/scripts/app/entities/livre/livre-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/livre/livre-dialog.controller.js
@@ -2,32 +2,38 @@
 
 angular.module('quinzaineDuLivreApp').controller('LivreDialogController',
     ['$scope', '$stateParams', '$modalInstance', 'entity', 'Livre', 'Age', 'Categorie', 'Editeur',
-        function($scope, $stateParams, $modalInstance, entity, Livre, Age, Categorie, Editeur) {
+        function ($scope, $stateParams, $modalInstance, entity, Livre, Age, Categorie, Editeur) {
 
-        $scope.livre = entity;
-        $scope.ages = Age.query();
-        $scope.categories = Categorie.query();
-        $scope.editeurs = Editeur.query();
-        $scope.load = function(id) {
-            Livre.get({id : id}, function(result) {
-                $scope.livre = result;
+            Categorie.query({}, function (result) {
+                $scope.categories = result;
             });
-        };
+            Age.query({}, function (result) {
+                $scope.ages = result;
+            });
+            Editeur.query({}, function (result) {
+                $scope.editeurs = result;
+            });
+            $scope.livre = entity;
+            $scope.load = function (id) {
+                Livre.get({id: id}, function (result) {
+                    $scope.livre = result;
+                });
+            };
 
-        var onSaveFinished = function (result) {
-            $scope.$emit('quinzaineDuLivreApp:livreUpdate', result);
-            $modalInstance.close(result);
-        };
+            var onSaveFinished = function (result) {
+                $scope.$emit('quinzaineDuLivreApp:livreUpdate', result);
+                $modalInstance.close(result);
+            };
 
-        $scope.save = function () {
-            if ($scope.livre.id != null) {
-                Livre.update($scope.livre, onSaveFinished);
-            } else {
-                Livre.save($scope.livre, onSaveFinished);
-            }
-        };
+            $scope.save = function () {
+                if ($scope.livre.id != null) {
+                    Livre.update($scope.livre, onSaveFinished);
+                } else {
+                    Livre.save($scope.livre, onSaveFinished);
+                }
+            };
 
-        $scope.clear = function() {
-            $modalInstance.dismiss('cancel');
-        };
-}]);
+            $scope.clear = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        }]);
