4066a4c6a56ba5eeb0b678485869089e832e7bb1
Florian Chauveau <f.chauveau@code-troopers.com>
2015-10-10 13:49:12 +0200

    plug search button

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -2,37 +2,43 @@
 
 angular.module('quinzaineDuLivreApp')
     .controller('MainController', function ($scope, Principal) {
-        Principal.identity().then(function(account) {
+        Principal.identity().then(function (account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
     })
     .controller('TableListeController', function ($scope, Principal, Livre, ParseLinks) {
+        $scope.filter = {};
         $scope.livres = [];
         $scope.page = 0;
-        $scope.loadAll = function() {
-            Livre.query({page: $scope.page, size: 20}, function(result, headers) {
+        $scope.loadAll = function () {
+            Livre.query({page: $scope.page, size: 20}, function (result, headers) {
                 $scope.links = ParseLinks.parse(headers('link'));
                 for (var i = 0; i < result.length; i++) {
                     $scope.livres.push(result[i]);
                 }
             });
         };
-        $scope.reset = function() {
+        $scope.reset = function () {
             $scope.page = 0;
             $scope.livres = [];
             $scope.loadAll();
         };
-        $scope.loadPage = function(page) {
+        $scope.loadPage = function (page) {
             $scope.page = page;
             $scope.loadAll();
         };
         $scope.loadAll();
+
+        $scope.search = function () {
+        //    probably loadAll() here
+        }
     })
-    .directive('filtreTable', function () {
+    .directive('booksFilter', function () {
         return {
             restrict: 'E',
-            templateUrl: 'scripts/app/main/filter.html'
+            templateUrl: 'scripts/app/main/filter.html',
+            controller: 'TableListeController'
         };
     })
     .directive('tableListe', function () {
